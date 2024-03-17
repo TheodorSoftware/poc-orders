de7ec71b6136f17304ef9b8f40dfd12e21001368
System Administrator <root@192-168-0-100.rdsnet.ro>
2024-03-17 23:26:44 +0200

    [feature][loginFlow][LoginPage structure created]

--- poc-orders/src/App.tsx
+++ poc-orders/src/App.tsx
@@ -1,13 +1,33 @@
-import { Fragment, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Fragment } from 'react'
 import './App.css'
+import { Router, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import LoginPage from './pages/login/LoginPage'
+import DashboardPage from './pages/dashboard/DashboardPage'
+
+const router = createBrowserRouter([
+  {
+    path: '/login',
+    element: <LoginPage />
+  },
+  {
+    path: '/register',
+    element:<h1> TODO : Register page </h1>
+  },
+  {
+    path: '/',
+    element: <DashboardPage />
+  },
+  {
+    path: '*',
+    element: <p> Page not found </p>
+  }
+])
 
 function App() {
-1
+
   return (
    <Fragment>
-    <h1> Salut </h1>
+    <RouterProvider router={router}/>
    </Fragment>
   )
 }
