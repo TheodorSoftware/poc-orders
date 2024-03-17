af9038ef61fb47cefec7af3d766f1870445d5b51
System Administrator <root@192-168-0-100.rdsnet.ro>
2024-03-17 21:57:26 +0200

    [first commit]

---
+++ poc-orders/src/App.tsx
@@ -0,0 +1,15 @@
+import { Fragment, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+1
+  return (
+   <Fragment>
+    <h1> Salut </h1>
+   </Fragment>
+  )
+}
+
+export default App
